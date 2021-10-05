c94ebb216b9a0d8610c0f03930e85a002feed315
FabCom <fabcom@communs.fr>
2021-10-05 08:39:58 +0200

    Delete old files

--- exo_07_a.rb
+++
@@ -1,5 +0,0 @@
-puts "Bonjour, c'est quoi ton blase ?"
-user_name = gets.chomp
-puts user_name
-
-# gets.chomp demande à l'utilisateur de taper quelque chose / input au clavier
--- exo_07_c.rb
+++
@@ -1,6 +0,0 @@
-user_name = gets.chomp
-puts user_name
-
-# Le programme de l'exo_07_b accompagne davantage l'utilisateur, apporte du contexte par rapport à l'exo_07_c et améliore la demande d'input en affichant un symbole au niveau de l'entrée >
-# puts affiche quelque chose puis fait un retour à la ligne
-# print affiche quelque chose mais ne fait pas de retour à la ligne
